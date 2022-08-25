4826d740dc0a19b2ecde44db165857c483ac9199
Ridwan Satria Wicaksana <wicaksana.rs@gmail.com>
2022-08-25 09:47:52 +0700

    first commit

---
+++ app/Http/Controllers/LoginControllerApi.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class LoginControllerApi extends Controller
+{
+    /**
+     * login
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::where('username', $request->username)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Login Failed!',
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Success!',
+            'data'    => $user,
+            'token'   => $user->createToken('user')->accessToken    
+        ]);
+    }
+}
